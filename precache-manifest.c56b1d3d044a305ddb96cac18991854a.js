06e877b603eee473a7e1b0bc59719f94c4886639
CipherProxy <admin@cipherproxy.com>
2021-01-04 18:05:07 -0500

    Updates

---
+++ precache-manifest.c56b1d3d044a305ddb96cac18991854a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1603bcf2c41d3c30280",
+    "url": "/testnet-cp-yield-farm/static/css/main.7f6ea087.chunk.css"
+  },
+  {
+    "revision": "a1603bcf2c41d3c30280",
+    "url": "/testnet-cp-yield-farm/static/js/main.a1603bcf.chunk.js"
+  },
+  {
+    "revision": "41248378dbd58bf6032a",
+    "url": "/testnet-cp-yield-farm/static/css/1.65bc998c.chunk.css"
+  },
+  {
+    "revision": "41248378dbd58bf6032a",
+    "url": "/testnet-cp-yield-farm/static/js/1.41248378.chunk.js"
+  },
+  {
+    "revision": "adb43e62dedcf6c56239",
+    "url": "/testnet-cp-yield-farm/static/js/runtime~main.adb43e62.js"
+  },
+  {
+    "revision": "700a98379a2689db8035b54c85b19532",
+    "url": "/testnet-cp-yield-farm/static/media/farmer.700a9837.png"
+  },
+  {
+    "revision": "d321147073cf16adc5a12f607d425f5e",
+    "url": "/testnet-cp-yield-farm/index.html"
+  }
+];
